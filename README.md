ef14f23ee63106ed83c737649f4343160ba8df65
Ivan Storck <ivanoats@gmail.com>
2014-07-16 11:31:39 -0700

    Force Directed Graph of WeightedQuickUnion still has problems, nodes are jammed in top left corner, no labels no unions yet

--- README.md
+++ README.md
@@ -5,20 +5,33 @@ them on a web page with [D3.js](http://d3js.org/).
 
 ## Prerequisites
 
-`npm -g install browserify node-static`
+[NodeJS](http://nodejs.org) installed.
+Node Packages:
+`npm -g install browserify node-static watchify`
 
 ## Install
-
+`git clone` (this repo's URL)
+`cd dsaJS`
 `npm install && bower install`
 
 ## Build
 
 `browserify public/app.js -t debowerify -o public/bundle.js`
 
+### Auto-Build
+`watchify public/app.js -t debowerify -o public/bundle.js -v`
+
 ## Run
 
 `static public -p3000` - this assumes you have `npm -g install node-static` as 
 a simple web server available.
 
+Visit [http://localhost:3000](http://localhost:3000) in your browser.
 
+# Contributing
 
+Send [Ivan Storck](https://github.com/ivanoats) a (clean) Pull Request on GitHub
+
+# License
+Copyright 2014 Code Fellows LLC 
+[MIT](http://opensource.org/licenses/MIT)
--- public/app.js
+++ public/app.js
@@ -4,7 +4,76 @@ window.d3 = require('d3');
 var WeightedQuickUnion = require('../lib/WeightedQuickUnion');
 
 var width  = 1024,
-    height = 1024,
+    height = 512,
     svg    = d3.select('#graph')
                .append('svg')
                .attr({width: width, height: height});
+
+var wqu = new WeightedQuickUnion(25);
+console.dir(wqu);
+
+// Visualize WQU with https://github.com/mbostock/d3/wiki/Tree-Layout
+// connect a site, revisualize
+var nodes = [];
+nodes = wqu.id;
+var edges = [];
+
+var dataset = {
+  nodes: nodes,
+  edges: edges
+};
+
+// force layout?
+var colors = d3.scale.category10();
+
+var force = d3.layout.force()
+                     .nodes(dataset.nodes)
+                     .links(dataset.edges)
+                     .linkDistance([50])
+                     .charge([-100])
+                     .size([width,height])
+                     .start();
+
+var edges = svg.selectAll('line')
+               .data(dataset.edges)
+               .enter()
+               .append('line')
+               .style('stroke', '#ccc')
+               .style('stroke-width', 4);
+
+var nodes = svg.selectAll('circle')
+  .data(dataset.nodes)
+  .enter()
+  .append('circle')
+  .attr('r', 10)
+  .style('fill', function(d, i) {
+    return colors(i);
+  })
+  .on('click', function(d){
+    alert("The Value is " + d);
+  })
+  .call(force.drag);
+/*
+var labels = svg.selectAll('text')
+                .data(dataset.nodes)
+                .enter()
+                .append('text')
+                .attr('id', 'tooltip')
+                .attr('font-family', 'sans-serif')
+                .attr('font-size', '11px')
+                .attr('font-weight', 'bold')
+                .attr('fill', 'black')
+                .text(function(d) { return d;} );
+*/
+force.on('tick', function() {
+  edges.attr('x1', function(d) { return d.source.x; })
+       .attr('y1', function(d) { return d.source.y; })
+       .attr('x2', function(d) { return d.target.x; })
+       .attr('y2', function(d) { return d.target.y; });
+
+  nodes.attr('cx', function(d) { return d.x; })
+       .attr('cy', function(d) { return d.y; });
+
+//  labels.attr('transform', function(d) { return 'translate(' +  (d.x + 10) +  ',' + d.y + ')'; } );
+
+});
