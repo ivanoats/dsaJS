7bdc6f5f5afbc3d0678a6eee7ac23b2821cc89b3
Ivan Storck <ivanoats@gmail.com>
2014-09-02 09:36:58 -0700

    Refactor instantiating a new LinkedListStack to a beforeEach block

--- test/linkedListStack-test.js
+++ test/linkedListStack-test.js
@@ -5,53 +5,50 @@ var LinkedListStack = require('../lib/linkedListStack');
 
 describe('Linked List Stack', function() {
 
+  var lls;
+
+  beforeEach(function() {
+    lls = new LinkedListStack();
+  });
+
   it('can instantiate a new linked list stack', function() {
-    var lls = new LinkedListStack();
     expect(lls).to.be.an.instanceof(LinkedListStack);
   });
 
   it('tests for an empty list and returns true', function () {
-    var lls = new LinkedListStack();
     expect(lls.isEmpty()).to.be.true;
   });
 
   it('can push an item and it is no longer empty', function() {
-    var lls = new LinkedListStack();
     lls.push('foo');
     expect(lls.isEmpty()).to.be.false;
   });
 
   it('can pop items', function () {
-    var lls = new LinkedListStack();
     lls.push('foo');
     expect(lls.pop()).to.be.equal('foo');
   });
 
   it('errors when popping an empty list',function() {
-    var lls = new LinkedListStack();
     expect(function(){lls.pop();}).to.throw(/is empty/);
   });
 
   it('can find a node',function() {
-    var lls = new LinkedListStack();
     lls.push('a'); lls.push('b'); lls.push('c');
     expect(lls.find('c')).to.have.property('item');
     expect(lls.find('c').item).to.equal('c');
   });
 
   it('can display the linked list', function() {
-    var lls = new LinkedListStack();
     lls.push('a'); lls.push('b'); lls.push('c');
     expect(lls.display()).to.equal('c\nb\na\n');
   });
 
   it('errors when finding on an empty list', function() {
-    var lls = new LinkedListStack();
     expect(function(){lls.find('item');}).to.throw(/is empty/);
   });
 
   it('can insert a new node after an item', function() {
-    var lls = new LinkedListStack();
     lls.push('a'); lls.push('b'); lls.push('c');
     lls.insert('b and a half','c');
     expect(lls.display()).to.equal('c\nb and a half\nb\na\n');
