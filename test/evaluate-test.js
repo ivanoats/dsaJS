b60dfda6a86072b9d1a70a73085a17fa7922a184
Ivan Storck <ivanoats@gmail.com>
2014-11-07 10:03:09 -0800

    removing debug logging

--- test/evaluate-test.js
+++ test/evaluate-test.js
@@ -6,7 +6,7 @@ var expect   = require('chai').expect;
 var evaluate = require(rootPath + '/lib/evaluate');
 
 describe('Evaluate', function() {
-  it('evaluates', function() {
+  it('evaluates an arithmatic expression separated by spaces with only * and +', function() {
     var expr = '( ( 1 + 2 ) * ( 3 + 4 ) )';
     expect(evaluate(expr)).to.equal(21);
   });
